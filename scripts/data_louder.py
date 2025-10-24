054ed4683faa170b12a7450a98b49d9c30d10512
Edison Gonzalez <139730204+EdisonGonzalezA@users.noreply.github.com>
2025-10-24 00:22:30 -0500

    clase_24/10/2025

--- scripts/data_louder.py
+++ scripts/data_louder.py
@@ -1,16 +1,7 @@
-# %%
 import pandas as pd
-import os
 
-# RUTA ABSOLUTA DE LA CARPETA DONDE ESTA EL SCRIPT (.../SCRIPT/)
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Ceonstruir la ruta del archivo csv de data
-DATA_PATH = os.path.join(SCRIPT_DIR, "..", "data", "games.csv")
 
 # creacion de funcion
-
-
 def cargar_datos(path):
     print(f"Cargando datos desde {path}...")
 
@@ -25,19 +16,3 @@ def cargar_datos(path):
     except Exception as e:
         print(f"Ocurrio un error inesperado {e}")
         return None
-
-
-# ¿Este archivo se esta ejecuntamndo directamente por el usuario o esta siendo importado por otro dscript?
-if __name__ == "__main__":
-    # indica donde esta el script actual
-    print(f"Ejecutando script desde:  {os.path.abspath(__file__)}")
-
-    # llama a la funcion de arriba para cargar el csv
-    dataframe_juegos = cargar_datos(DATA_PATH)
-
-    if dataframe_juegos is not None:
-        print("\n---Primeras 5 filas---")
-        print(dataframe_juegos.head())
-
-        print("\n---Informacion del Dataframe---")
-        dataframe_juegos.info(show_counts=True)
