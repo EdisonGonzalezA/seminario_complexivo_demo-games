f78430b4acd8961f0e482a5fc43e20d1bd400f2b
Edison Gonzalez <139730204+EdisonGonzalezA@users.noreply.github.com>
2025-11-01 20:48:52 -0500

    clase 30/10/2025

--- app_demo_4.py
+++ app_demo_4.py
@@ -26,9 +26,52 @@ if st.button("Predecir datos"):
 
 st.header("Selección única: Selectbox vs. Radio")
 
-opcion_sb = st. selectbox(" i Escoge una opción! (Selectbox)", [
-                          "Opción A", "Opción B", "Opción C"])
+# st.selectbox
+opcion_sb = st.selectbox(" ¡Escoge una opción! (Selectbox)", [
+    "Opción A", "Opción B", "Opción C"])
 
+# st.radio
 opcion_r = st.radio("¡Escoge una opcion! (Radio)", [
-                    "Opción A", "Opción B", "Opción C"])
-st.write(f"Selectbox: {opcion_sb}")
+    "Opción A", "Opción B", "Opción C"])
+st.write(f"Selectbox: {opcion_sb}, Radio: {opcion_r}")
+
+# st.multiselect
+opciones_multi = st.multiselect(
+    "Elige tu genero de videojuego favorito:",
+    ["Accion", "Puzzle", "Stragy", "Shooter"],
+    default=["Accion"]
+)
+st.write(f"Sewleccionado: {opciones_multi}")
+
+# st.slider con dos valores
+st.header("Seleccion de rango: Slider tipo tupla")
+
+rango_anios = st.slider(
+    "Seleccioona un rango de años",
+    # valor minimo, valor maximo, default del rango
+    2000, 2023, (2010, 2015)
+)
+st.write(f"eL sLIDER DEVUELVE UNA TUPLA: {rango_anios}")
+
+
+# st.code
+st.write("Aqui estoy mosdtrando un codigo de Pandas")
+st.code(
+    "pd.read_csv(ruta)",
+    language="Python"
+)
+
+# Elementos de notificaciones
+st.header("Notificacxiones")
+
+# Notificaciones visuales
+st.success("Los datos fueron cargadoos correctamente")
+st.warning("El dataset tiene valores nulos")
+st.error("No se pudo conectar a la base de datos")
+
+# st.metric
+st.metric(
+    label="Ventas Totales",
+    value="$1,220,000",
+    delta="$50,000 vs mes anterior"
+)
